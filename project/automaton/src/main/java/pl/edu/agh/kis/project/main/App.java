b6b0bf34b11d1b86cdd9c1bdfd37f8f07aab3495
Kamil Osuch <Kamil Osuch>
2015-11-23 17:18:40 +0100

    initial commit

---
+++ project/automaton/src/main/java/pl/edu/agh/kis/project/main/App.java
@@ -0,0 +1,7 @@
+package pl.edu.agh.kis.project.main;
+
+public class App {
+    public static void main(String[] args) {
+        // TODO
+    }
+}
---
+++ project/automaton/src/test/java/pl/edu/agh/kis/project/main/AppTest.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.kis.project.main;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void mainTest() {
+        // TODO
+    }
+
+}
