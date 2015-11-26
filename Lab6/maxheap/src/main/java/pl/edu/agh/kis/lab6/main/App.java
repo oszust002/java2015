e630f481a75b6865df98827cd3cc141ef097d1c1
Kamil Osuch <Kamil Osuch>
2015-11-26 19:41:37 +0100

    initial commit

---
+++ Lab6/maxheap/src/main/java/pl/edu/agh/kis/lab6/main/App.java
@@ -0,0 +1,7 @@
+package pl.edu.agh.kis.lab6.main;
+
+public class App {
+    public static void main(String[] args) {
+        // TODO
+    }
+}
---
+++ project/automaton/src/main/java/pl/edu/agh/kis/project/main/Automaton.java
@@ -0,0 +1,7 @@
+package pl.edu.agh.kis.project.main;
+
+/**
+ * Created by Kamil on 23.11.2015.
+ */
+public class Automaton {
+}
