915f8e4a1606bcc6fe0f3686f1b31a08bb2e111e
Kamil Osuch <Kamil Osuch>
2015-11-27 11:21:19 +0100

    initial commit

---
+++ Lab7/zerowe/src/main/java/pl/edu/agh/kis/lab7/main/App.java
@@ -0,0 +1,7 @@
+package pl.edu.agh.kis.lab7.main;
+
+public class App {
+    public static void main(String[] args) {
+        // TODO
+    }
+}
---
+++ Lab7/zerowe/src/test/java/pl/edu/agh/kis/lab7/main/AppTest.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.kis.lab7.main;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void mainTest() {
+        // TODO
+    }
+
+}
